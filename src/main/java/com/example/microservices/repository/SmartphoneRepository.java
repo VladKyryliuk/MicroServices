2ad6bdf3b61dcd7ac6d3dd2edf8c79c07c50eb2c
VladKyryliuk <125196452+VladKyryliuk@users.noreply.github.com>
2023-09-16 22:19:09 +0300

    Second HomeWork Connect to MongoDB and changed CRUD methods

--- src/main/java/com/example/microservices/repository/SmartphoneRepository.java
+++ src/main/java/com/example/microservices/repository/SmartphoneRepository.java
@@ -1,8 +1,10 @@
 package com.example.microservices.repository;
 
 import com.example.microservices.model.Smartphone;
+import com.mongodb.lang.NonNull;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface SmartphoneRepository extends MongoRepository<Smartphone,String> {
+    @NonNull
     void deleteById(String id);
 }
